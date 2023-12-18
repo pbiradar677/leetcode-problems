5ec0930b356c63c9279ef6379b8d038c39a68661
Parshuram <parshuram@blitzcodesys.com>
2023-12-19 00:04:49 +0530

    array prototype last

---
+++ Day 24/last.js
@@ -0,0 +1,8 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function () {
+  return this.length ? this.at(-1) : -1;
+};
+const arr = [1, 2, 3];
+console.log(arr.last()); // 3
