ae19e0826a80779c4e23d124ef756b3e2237b26f
Parshuram <parshuram@blitzcodesys.com>
2024-01-11 22:37:30 +0530

    call fun with custom context

---
+++ Day 39/call_fun_custom_context.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Object} context
+ * @param {Array} args
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Function.prototype.callPolyfill = function (context, ...args) {
+  Object.defineProperty(context, "fn", {
+    value: this,
+    enumerable: false, // default false
+  });
+  return context.fn(...args);
+};
+
+function increment() {
+  this.count++;
+  return this.count;
+}
+let result = increment.callPolyfill({ count: 10 });
+
+console.log(result); // 2
